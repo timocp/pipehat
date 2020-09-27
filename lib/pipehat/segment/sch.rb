b802a301838f337ffd3f1e0695372daea22ebc08
Tim Peters <tim@catminion.net>
2020-09-27 19:44:06 +0930

    Add SCH (scheduling activity information) segment

---
+++ lib/pipehat/segment/sch.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Pipehat
+  module Segment
+    # Scheduling Activity Information
+    class SCH < Base
+      add_field :placer_appointment_id,         :EI
+      add_field :filler_appointment_id,         :EI
+      add_field :occurrence_number,             :NM
+      add_field :placer_group_number,           :EI
+      add_field :schedule_id,                   :CWE
+      add_field :event_reason,                  :CWE
+      add_field :appointment_reason,            :CWE
+      add_field :appointment_type,              :CWE
+      add_field :appointment_duration,          :NM
+      add_field :appointment_duration_units,    :CNE
+      add_field :appointment_timing_quantity,   :ST
+      add_field :placer_contact_person,         :XCN
+      add_field :placer_contact_phone_number,   :XTN
+      add_field :placer_contact_address,        :XAD
+      add_field :placer_contact_location,       :PL
+      add_field :filler_contact_person,         :XCN
+      add_field :filler_contact_phone_number,   :XTN
+      add_field :filler_contact_address,        :XAD
+      add_field :filler_contact_location,       :PL
+      add_field :entered_by_person,             :XCN
+      add_field :entered_by_phone_number,       :XTN
+      add_field :entered_by_location,           :PL
+      add_field :parent_placer_appointment_id,  :EI
+      add_field :parent_filler_appointment_id,  :EI
+      add_field :filler_status_code,            :CWE
+      add_field :placer_order_number,           :EI
+      add_field :filler_order_number,           :EI
+    end
+  end
+end
